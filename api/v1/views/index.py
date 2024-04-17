ce47ffb774f7b22f4dd1107f7ca981a502131f09
Mamuche <marionlaroche17@gmail.com>
2024-04-17 11:38:46 +0200

    task5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
@@ -11,3 +18,14 @@ def get_status():
     }
     """converts the dictionary into a JSON response"""
     return jsonify(status)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    stats = {'amenities': storage.count(Amenity),
+             'cities': storage.count(City),
+             'places': storage.count(Place),
+             'reviews': storage.count(Review),
+             'states': storage.count(State),
+             'users': storage.count(User)}
+    return jsonify(stats)
